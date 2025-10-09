884bff759415c49bc66cbbe713385d66e4cb5917
PNL\buts930 <nathan.butschli@pnnl.gov>
2025-10-09 09:44:03 -0700

    added TODOs, uncommented logging code.

--- src/main.py
+++ src/main.py
@@ -11,9 +11,13 @@ logging.basicConfig(level=config.LOG_LEVELS.get(config.LOG_LEVEL),
         logging.StreamHandler()
     ]
 )
+logger = logging.getLogger(config.LOG_NAME)
 
 app = FastAPI(title="Math MCP Service")
 
+#TODO: Root endpoint currently not accessible, when run as an MCP service.
+#      MCP endpoints not accessible when run as FastAPI service.
+#      Need to consolodate them properly.
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to the Math MCP Service!"}
@@ -23,24 +27,22 @@ mcp = FastMCP("Math")
 @mcp.tool()
 async def add(a: int, b: int) -> dict:
     """Add two numbers"""
-    #logger = logging.getLogger(config.LOG_NAME)
-    #logger.info(f"Adding {a} and {b}")
+    logger.info(f"Adding {a} and {b}")
     c = a + b
-    #logger.info(f"Result of addition: {c}")
+    logger.info(f"Result of addition: {c}")
     return {"result": c}
 
 @mcp.tool()
 async def multiply(a: int, b: int) -> dict:
     """Multiply two numbers"""
-    #logger = logging.getLogger(config.LOG_NAME)
-    #logger.info(f"Multiplying {a} and {b}")
+    logger.info(f"Multiplying {a} and {b}")
     c = a * b
-    #logger.info(f"Result of multiplication: {c}")
+    logger.info(f"Result of multiplication: {c}")
     return {"result": c}
 
 mcp_app = mcp.http_app(path ='/mcp')
 app = FastAPI(title="Math MCP Service", lifespan=mcp_app.lifespan)
 app.mount("/mcp", mcp_app)
 
-# if __name__ == "__main__":
-#     mcp.run(transport="streamable-http", host=config.SERVER_HOST, port=config.SERVER_PORT)
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http", host=config.SERVER_HOST, port=config.SERVER_PORT)
